3339b186e1713fddb9ed807bf7d32ac97b541f15
corey buckley <coreybuckley13@gmail.com>
2019-07-08 23:49:28 -0400

    Mercury in thermometer scales to temp now.

--- src/components/Temperature.vue
+++ src/components/Temperature.vue
@@ -5,6 +5,7 @@
   - More data? Kind of like the compactness though.
   - Have colors change on time of day or weather condition?
   - Small animations of weather condition
+  - Maybe indicate time or night / day somehow?
 -->
 
 <template>
@@ -12,12 +13,26 @@
     <svg>
       <circle cx="15" cy="150" r="20" class="no-top-stroke"/>
       <rect x="0" y="-10" ry="5" height="150" width="30" class="no-bottom-stroke"/>
-      <rect x="2" y="10.7" ry="5" height="130" width="26" class="temp-indicator-bar"/>
+      <!-- <rect x="2" y="10.7" ry="5" height="130" width="27" class="temp-indicator-bar"/> -->
+      <rect x="1" :y="130 - Math.min(150 * (temp / MAX_THERM_TEMP), 150) + 10.7" ry="5" :height="Math.min(150 * (temp / MAX_THERM_TEMP), 150)" width="28" class="temp-indicator-bar"/>
     </svg>
     <div class="temp-info-container w-100">
       <div class="temp-display">
-        {{ temperature }}<span v-if="unit === 'C'">&#8451;</span>
-                        <span v-else-if="unit === 'F'">&#8457;</span>
+        <!-- Width varies depending on unit. Kelvin values
+             typically larger (hundreds) so needs more width.
+        -->
+        <div
+        :class="unit === 'C' || unit === 'F' ? 'temp-number-imperial-or-metric' : 'temp-number-kelvin'"
+        >
+          {{ temp | round }}
+        </div>
+        <div class="unit-container"
+             v-on:click="getTemperature"
+        >
+          <span v-if="unit === 'C'" class="unit-display">&#8451;</span>
+          <span v-else-if="unit === 'F'" class="unit-display">&#8457;</span>
+          <span v-else-if="unit === 'K'" class="unit-display">&#8490;</span>
+        </div>
       </div>
       <div class="temp-image-icons">
         <img class="weather-img"
@@ -53,22 +68,10 @@ export default {
     }
   },
   computed: {
-    temperature: function () {
-      if (this.unit === 'F') {
-        this.unit = 'C';
-        this.temp = Math.round((this.temp - 32) / 1.8);
-      }
-      else if (this.unit === 'C') {
-        this.unit = 'F';
-        this.temp = Math.round(this.temp * 1.8 + 32);
-      }
-      else if (this.unit === 'K') {
-        //this.unit = 'C';
-        //this.temp = Math.floor(this.temp - 273.15);
-        this.unit = 'F';
-        this.temp = Math.round(this.temp * 1.8 - 459.67);
-      }
-      return this.temp;
+    // Returns the maximum temperature the
+    // temperature indicator bar will scale up to (in fahrenheit)
+    MAX_THERM_TEMP () {
+      return 105;
     }
   },
   mounted () {
@@ -81,11 +84,13 @@ export default {
     updateData: function () {
       window.navigator.geolocation.getCurrentPosition(pos => {
         fetch(WEATHER_ENDPOINT + `lat=${pos.coords.latitude}&lon=${pos.coords.longitude}`).then(result => {
+          // TODO On update the temp unit will change to fahrenheit. Change so it adjusts to whatever
+          //      unit is set (if there is one, else default to F)
           result.json().then(json => {
             // OpenWeatherMap API returns temp in Kelvin by default.
-            // Kelvin -> Celsius
-            this.unit = 'K';
-            this.temp = json.main.temp;
+            // Kelvin -> Fahrenheit
+            this.temp = json.main.temp * 1.8 - 459.67;
+            this.unit = 'F';
             this.weather_items = json.weather;
             this.city = json.name;
             console.log(json.weather);
@@ -93,6 +98,30 @@ export default {
           }).catch(err => console.log(err));
         }).catch(err => console.log(err));
       }, err => console.log(err));
+    },
+    getTemperature: function () {
+      if (this.unit === 'F') {
+        this.unit = 'C';
+        this.temp = (this.temp - 32) / 1.8;
+      }
+      else if (this.unit === 'C') {
+        // this.unit = 'F';
+        // this.temp = Math.round(this.temp * 1.8 + 32);
+        this.unit = 'K';
+        this.temp = this.temp + 273.15;
+      }
+      else if (this.unit === 'K') {
+        //this.unit = 'C';
+        //this.temp = Math.floor(this.temp - 273.15);
+        this.unit = 'F';
+        this.temp = this.temp * 1.8 - 459.67;
+      }
+      return this.temp;
+    }
+  },
+  filters: {
+    round: function(value) {
+      return Math.round(value);
     }
   }
 }
@@ -108,7 +137,8 @@ export default {
   min-width: 7em;
   height: 10em;
   margin: auto;
-  border: solid 1px rgba(60, 18, 247, 0.5);
+  /* border: solid 1px rgba(60, 18, 247, 0.5); */
+  border: solid 2px black;
   border-radius: 2px;
   box-shadow: 3px 2px rgba(0,0,0,.3);
   /* Cold */
@@ -117,33 +147,57 @@ export default {
   /* linear-gradient(to bottom right, rgba(234, 20, 20, 0.651), rgba(255, 165, 0, 0.527)) */
   /* Temperate / Cloudy */
   /* linear-gradient(to bottom right, rgba(144, 151, 197, 0.651), rgba(11, 23, 112, 0.527)) */
-  background-image: linear-gradient(to bottom right, rgba(236, 14, 155, 0.8), rgba(199,21,133, 0.6));
+  /*background-image: linear-gradient(to bottom right, rgba(236, 14, 155, 0.8), rgba(199,21,133, 0.6));*/
+  background-image: linear-gradient(to top right, rgba(0, 55, 255, 0.8), rgba(21, 177, 199, 0.6));
 }
 .temp-info-container {
-  
+  display: flex;
+  flex-flow: row wrap;
 }
 .w-100 {
   width: 100%;
 }
 .temp-display {
+  display: flex;
   position: relative;
   font-size: 30px;
+  top: 5px;
   font-weight: bolder;
   color: white
+}
+.temp-number-imperial-or-metric {
+  width: 37px;
+}
+.temp-number-kelvin {
+  width: 49px;
 }
 .address-lbl {
   color: white;
   text-shadow: 1px 1px black;
+  font-weight: bold;
   padding-top: 60px;
   padding-left: 2px;
 }
 .weather-img {
-  /* position: relative;
-  width: 50px;
-  top: -112px;
-  left: 25px; */
   width: 35px;
   height: 42px;
+}
+.unit-container {
+  display: inline-block;
+  background-color: rgba(100,100,100,0.4);
+  border: solid rgba(100,100,100,0.4) 1px;
+  border-radius: 4px;
+  margin-left: 2px;
+  position: relative;
+  top: -0.5px;
+  box-shadow: 1px 2px rgba(0,0,0,0.3);
+  cursor: pointer;
+}
+.unit-container:hover {
+  text-shadow: black 2px 2px;
+}
+.unit-display {
+  transition: text-shadow 0.3s;
 }
 rect {
   fill: rgba(206, 191, 191, 0.493);
@@ -166,4 +220,8 @@ rect.temp-indicator-bar {
   fill: red;
   stroke: none;
 }
+/* rect.temp-indicator-bar-full {
+  height: 150px;
+  y: -9.3;
+} */
 </style>
